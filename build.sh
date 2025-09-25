bda79ddf0d92f70792716a915aef2b5b2243ed71
Iasmin Vieira <iasminv.gd@gmail.com>
2025-09-25 17:29:07 -0300

    feat: Adiciona script para popular o banco de dados no deploy

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# 1. Instala as dependências
 pip install -r requirements.txt
 
+# 2. Aplica as migrações do banco (cria as tabelas)
 alembic upgrade head
+
+# 3. POPULA O BANCO DE DADOS (APENAS DESTA VEZ)
+echo "Iniciando a população do banco de dados..."
+python populate_db.py
