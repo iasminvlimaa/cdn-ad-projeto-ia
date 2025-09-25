c201750be6e5fffdf06349c2ae6086a9c331c8f5
Iasmin Vieira <iasminv.gd@gmail.com>
2025-09-25 17:25:16 -0300

    Resolvendo bugs

--- app/db/models.py
+++ app/db/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from app.db.base import Base
 
 class Escola(Base):
     __tablename__ = "escolas"
